78c8525711795c94f7c0338193672444cc22768d
Sumit Singh <126954034+Sumitx5@users.noreply.github.com>
2026-09-06 16:33:45 +0530

    Add files via upload

--- leaf.py
+++ leaf.py
@@ -1,27 +1,34 @@
-import discord
-from discord.ext import commands
-import os
-import config
-import asyncio
+import discord, os, json, config, asyncio, random
+from discord.ext import commands, tasks
+config_file = "data/config.json"
 
 intents = discord.Intents.all()
-bot= commands.Bot(command_prefix='!', intents=intents)
-bot.remove_command("help")
+bot = commands.Bot(command_prefix="!", intents=intents)
+members = len(bot.users)
+guilds = len(bot.guilds)
+activities_list = [
+    discord.Activity(type=discord.ActivityType.listening, name="!help", state= "Playing With Python"),
+    discord.Activity(type=discord.ActivityType.watching, name=f"{members} Members & {guilds} Guilds")
+   ]
+@tasks.loop(seconds=5)
+async def change_activity():
+    activity = random.choice(activities_list)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
 @bot.event
 async def on_ready():
-    activity = discord.Game(name="Leaf X PYTHON", type=3)
-    await bot.change_presence(status=discord.Status.idle, activity=activity)
-    print(f'I am Online')
+    print(f'Logged in as Leaf!')
+    try:
+        for filename in os.listdir("./cmds"):
+            if filename.endswith(".py"):
+                await bot.load_extension(f"cmds.{filename[:-3]}")
+                print(f"Loaded cog: {filename[:-3]}")
+        print("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x--x-x-x-x-x")
+        print("Loaded all cogs successfully.")
+    except Exception as e:
+        print(f"{e}")
     await bot.tree.sync()
+    change_activity.start()
 
-async def Load():
-    for filename in os.listdir("./cmds"):
-        if filename.endswith(".py"):
-            await bot.load_extension(f"cmds.{filename[:-3]}")
-            print(f'Loaded Files {filename[:-3]}')
-
-async def main():
-    async with bot:
-        await Load()
-        await bot.start(config.Token)
-asyncio.run(main())
+if __name__ == "__main__":
+    bot.run(config.Token)
